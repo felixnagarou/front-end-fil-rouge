4344fe123cd649beb32686f29a5af9e2c09c8a01
M2i_Beffroi <mon.email@exemple.fr>
2023-11-16 14:26:15 +0100

    detailts

--- src/app/components/episode-details/episode-details.component.ts
+++ src/app/components/episode-details/episode-details.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { NavigationServiceService } from '../../services/navigation-service.service'; 
+import { EpisodeService } from './EpisodeService'; 
 
 @Component({
   selector: 'app-episode-details',
   templateUrl: './episode-details.component.html',
   styleUrls: ['./episode-details.component.css']
 })
-export class EpisodeDetailsComponent {
+export class EpisodeDetailsComponent implements OnInit, OnChanges {
+
+  @Input() episodeName: string; 
+  currentEpisode: any; 
+
+  constructor(private nav: NavigationServiceService, private episodeService: EpisodeService) {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.episodeName && !changes.episodeName.firstChange) {
+      this.getEpisodeDetails();
+    }
+  }
+
+  private getEpisodeDetails(): void {
+    const that = this;
+
+    this.episodeService
+      .getEpisodeDetails(this.episodeName)
+      .subscribe({
+        next(value) {
+          that.currentEpisode = value;
+        },
+        error(err) {
+          console.error(err);
+          that.currentEpisode = null;
+        },
+      });
+  }
 
 }
--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
-import {NavigationServiceService} from "../../services/navigation-service.service";
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { NavigationServiceService } from "../../services/navigation-service.service";
+import { MovieService } from './MovieService'; 
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css']
 })
-export class MovieDetailsComponent {
-  constructor(private nav: NavigationServiceService) {
+export class MovieDetailsComponent implements OnInit, OnChanges {
+  @Input() movieName: string;
+  currentMovie: any; 
 
+  constructor(private nav: NavigationServiceService, private movieService: MovieService) {
+    
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.movieName && !changes.movieName.firstChange) {
+      this.getMovieDetails();
+    }
+  }
+
+  private getMovieDetails(): void {
+    const that = this;
+
+    this.movieService
+      .getMovieDetails(this.movieName) 
+      .subscribe({
+        next(value) {
+          that.currentMovie = value;
+        },
+        error(err) {
+          console.error(err);
+          that.currentMovie = null;
+        },
+      });
+  }
 }
